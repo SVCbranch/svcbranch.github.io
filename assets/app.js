f21db0e07301ae32568d8015d5c9f958a3ffa825
SVCbranch <itPCS@ik.me>
2025-04-02 11:43:01 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,26 @@ if ('serviceWorker' in navigator) {
         .then(reg => console.log('Service Worker enregistré', reg))
         .catch(err => console.error('Erreur Service Worker', err));
 }
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+
+    // Afficher la boîte de dialogue immédiatement
+    showInstallPrompt();
+});
+
+function showInstallPrompt() {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('L’utilisateur a installé l’application');
+            } else {
+                console.log('L’utilisateur a refusé');
+            }
+            deferredPrompt = null;
+        });
+    }
+}
