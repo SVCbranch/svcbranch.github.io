2e6bc43b71bf5b9ea13e09fceb59472bdedab22c
SVCbranch <itPCS@ik.me>
2025-04-04 16:40:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,41 @@
 const CACHE_NAME = 'cacheapp';
 const URLS_TO_CACHE = [
+    '/',
     '/index.html',
     '/comment-ca-marche.html',
-    '/ou-utiliser.html',
-    '/app.js',
+    '/assets/app.js',
     '/assets/favicon.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Mise en cache des fichiers');
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Mise en cache des ressources');
             return cache.addAll(URLS_TO_CACHE);
         })
     );
 });
 
-// Intercepter les requêtes et servir les fichiers depuis le cache
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request).then(networkResponse => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
         })
     );
 });
 
-// Mettre à jour le cache si une nouvelle version est disponible
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.map((cache) => {
+                cacheNames.map(cache => {
                     if (cache !== CACHE_NAME) {
-                        console.log('Suppression de l’ancien cache:', cache);
+                        console.log('Suppression de l\'ancien cache', cache);
                         return caches.delete(cache);
                     }
                 })
